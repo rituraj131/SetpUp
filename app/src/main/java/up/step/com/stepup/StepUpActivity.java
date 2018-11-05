c06ee7997c6e7f12520abd6687d20cffb1547e2b
Ritu Raj <rituraj131@tamu.edu>
2018-11-05 16:08:33 -0600

    first commit

---
+++ app/src/main/java/up/step/com/stepup/StepUpActivity.java
@@ -0,0 +1,13 @@
+package up.step.com.stepup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StepUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_up);
+    }
+}
